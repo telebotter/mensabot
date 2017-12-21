2b1820f3a045acdaa8782b049358279a7e6e23f6
sarbot <mail@sarbot.de>
2017-12-21 13:50:06 +0100

    code style and comments added

--- mensa_bot.py
+++ mensa_bot.py
@@ -38,7 +38,7 @@ def main():
     Context.strings = dict(cfg.items('strings'))
     # following lines could just be: private_token = 'your-token'
     prvt = configparser.ConfigParser()
-    prvt.read('private.ini',encoding='UTF8')
+    prvt.read('private.ini', encoding='UTF8')
     private_token = prvt.get('private', 'token')
     Context.admin_id = int(prvt.get('private', 'admin_id'))
 
