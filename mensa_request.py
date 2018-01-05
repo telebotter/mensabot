4f812effc2bdedf3fe5aaf39d24156d28ea84cd5
hendricokakico <hendrik@war-im-urlaub.de>
2018-01-05 15:41:48 +0100

    useless function deleted

--- mensa_request.py
+++ mensa_request.py
@@ -22,10 +22,6 @@ def datetime_plus_days(date,plusdays):
     dateplusdays = date + datetime.timedelta(days= int(plusdays))
     return dateplusdays
 
-# not in use
-# def timedelta_to_hours(td):
-#     '''input a timedelta. output the timedelta in hours'''
-#     return td.days*24 + td.seconds//3600
 
 def get_food(date,canteen_1=167,canteen_2=201):
     wanted_date = datetime.datetime.strftime(date, '%Y-%m-%d')
